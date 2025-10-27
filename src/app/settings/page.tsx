cd39079cfc8e82f7488a62f06aad32a7e3b9a49e
malvis Anochie <malvisanochie97@gmail.com>
2025-10-27 20:26:51 +0100

    updated

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -13,6 +13,17 @@ import {
 } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 import { ModeToggle } from "@/components/ui/modetoggle";
+
+import * as React from "react";
+import { Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function SettingsPage() {
   const [currency, setCurrency] = useState("USD");
   const [language, setLanguage] = useState("en");
@@ -164,7 +175,26 @@ export default function SettingsPage() {
                   <SelectItem value="auto">Auto (System)</SelectItem>
                 </SelectContent>
               </Select> */}
-              <ModeToggle />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>{" "}
             </div>
           </Card>
         </div>
